5cf6ddebd32b6d6037651e3c74ca78b8fdc36daa
Mike Moore <tigerhollowpoint@gmail.com>
2018-02-22 22:52:46 -0500

    Update runner.php

--- runner.php
+++ runner.php
@@ -1,6 +1,7 @@
 <?php
 date_default_timezone_set('America/New_York');
 $posts = new SimpleXMLElement(file_get_contents('allPosts.xml'));
+$utc_time = new DateTimeZone('UTC');
 $itemArray = [];
 
 foreach($posts->post as $p) {
@@ -13,13 +14,16 @@ foreach($posts->post as $p) {
    if($k !== 'date') {
      continue;
    }
-   $strdate = strtotime($a);
+   $ny_time = new DateTime($a);
+   $ny_time->setTimezone($utc_time);
+   $pubDate = $datetime->format('D, d M Y H:i:s').' +0000';
    exit;
   }
-  //2018-02-22 08:10:00 convert time
-  $pubDate = date("", $strdate);
+  
 }
-                              
+
+echo '<?xml version="1.0" encoding="UTF-8" ?><rss version="2.0" xmlns:excerpt="http://wordpress.org/export/1.2/excerpt/" xmlns:content="http://purl.org/rss/1.0/modules/content/" xmlns:wfw="http://wellformedweb.org/CommentAPI/" xmlns:dc="http://purl.org/dc/elements/1.1/" xmlns:wp="http://wordpress.org/export/1.2/" ><channel>'..'</channel></rss>';
+die();
                               
 
 
