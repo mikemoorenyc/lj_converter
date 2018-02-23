1a015af6ba4f2c41c4919b69414402b8f1caca30
Mike Moore <tigerhollowpoint@gmail.com>
2018-02-22 22:36:04 -0500

    Update runner.php

--- runner.php
+++ runner.php
@@ -1,1 +1,27 @@
+<?php
+date_default_timezone_set('America/New_York');
+$posts = new SimpleXMLElement(file_get_contents('allPosts.xml'));
+$itemArray = [];
 
+foreach($posts->post as $p) {
+ $title = $p->title;
+  $tagArray = [];
+  foreach($p->tags->tag as $t) {
+   $tagArray[] = $t; 
+  }
+  foreach($p->attributes() as $k => $a) {
+   if($k !== 'date') {
+     continue;
+   }
+   $strdate = strtotime($a);
+   exit;
+  }
+  //2018-02-22 08:10:00 convert time
+  $pubDate = date("", $strdate);
+}
+                              
+                              
+
+
+
+?>
