55ade84a8a0291aab1ef9f0ccc598a5b8c5101c6
Mike Moore <tigerhollowpoint@gmail.com>
2018-02-23 09:20:46 -0500

    index.php

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
+	<?php /* How to display standard posts and search results */ ?>
+
+	<div class="post">
+
+
+		<a href="<?php the_permalink(); ?>" rel="bookmark">
+			<h2><?php the_title(); ?></h2>
+		</a>
+		<?php the_content(); ?>
+
+	</div>
+
+
+<?php endwhile; // End the loop. Whew. ?>
